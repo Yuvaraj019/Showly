a0369fa45c66f9d39e759a2358474e1466b5fab7
Yuvaraj <134210431+Yuvaraj019@users.noreply.github.com>
2025-07-18 14:23:21 +0530

    Add files via upload

---
+++ Webpage/Script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector(".search-box input");
+  const cards = document.querySelectorAll(".anime-card");
+
+  input.addEventListener("input", () => {
+    const query = input.value.toLowerCase();
+
+    cards.forEach(card => {
+      const title = card.querySelector(".anime-title").textContent.toLowerCase();
+      card.style.display = title.includes(query) ? "block" : "none";
+    });
+  });
+});
